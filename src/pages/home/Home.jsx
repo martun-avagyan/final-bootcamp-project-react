56007879e56e9c92db3924371317295f01e0cb16
arminasi <armanminasyanc@gmail.com>
2022-05-23 10:16:17 +0400

    feature(Home): update Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,43 @@
-import React from "react"
-import Slider from "../../components/slider/Slider"
+import React from "react";
+import { useState, useEffect } from "react";
+import Slider from "../../components/slider/Slider";
+import Nav from "../../components/navbar/Nav";
+import Board from "../../components/board/Board";
+import ItemSelector from "../../components/ItemSelector/ItemSelector";
 
 function Home() {
+
+	const [isVisible, setIsVisible] = useState(true);
+	const [height, setHeight] = useState(0);
+
+	useEffect(() => {   
+		window.addEventListener("scroll", listenToScroll);
+		return () => 
+			window.removeEventListener("scroll", listenToScroll); 
+	}, []);
+
+	const listenToScroll = () => {
+		let heightToHideFrom = 42;
+		const winScroll = document.body.scrollTop || 
+        document.documentElement.scrollTop;
+		setHeight(winScroll);
+
+		if (winScroll > heightToHideFrom) {  
+			isVisible && setIsVisible(false);
+		} else {
+			setIsVisible(true);
+		}  
+	};
+
 	return (
 		<div>
+			<Nav />
+			{height}
+			{isVisible && <ItemSelector />}
 			<Slider />
+			<Board />
 		</div>
-	)
+	);
 }
 
-export default Home
+export default Home;
