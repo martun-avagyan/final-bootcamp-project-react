7159e58e9dc4e176cf34bcc9e5967ce718057528
arminasi <armanminasyanc@gmail.com>
2022-05-23 10:08:22 +0400

    feat(Slider): add Slider component

--- src/components/slider/Card.styled.js
+++ src/components/slider/Card.styled.js
@@ -1,17 +1,14 @@
-.Slider {
-	box-sizing: border-box;
-	padding: 20px;
-	align-items: center;
-}
+import styled from "styled-components";
 
-.card {
+const Card  = styled.div`
 	pointer-events: none;
 	border: 1px solid rgb(221, 221, 221);
   width: 100%;
   height: 200px;
   font-size: 30px;
-	margin: 0 5px;
   display: flex;
   justify-content: center;
   align-items: center;
-}
+`;
+
+export default Card;
